00297116a4dfbebd0ac3564cef3aec5381ee37f5
btwthibo@gmail.com <btwthibo@gmail.com>
2023-03-16 11:40:09 +0100

    commit

--- index.js
+++ index.js
@@ -28,5 +28,5 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Listening on http://localhost:3000");
+  console.log("Development branch");
 });
